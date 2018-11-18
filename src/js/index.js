25e036c70b401802871c03b8ffe360155539ccf8
PhilT <phil@xceptionale.com>
2018-11-18 16:40:22 +1000

    Update

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,53 @@
+var Bayes  = require('testistic-bayes')
 
+var relationships2 = [
+{
+  id: 4,
+  description: 'test4',
+  QS: 25,
+  QF: 25,
+  PS: 25,
+  PF: 25
+},
+{
+  id: 5,
+  description: 'test5',
+  QS: 10,
+  QF: 40,
+  PS: 30,
+  PF: 20
+}]
+
+var relationships = [{
+  id: 1,
+  description: 'test1',
+  QS: 18,
+  QF: 2,
+  PS: 22,
+  PF: 58
+}, {
+  id: 2,
+  description: 'test2',
+  QS: 16,
+  QF: 4,
+  PS: 14,
+  PF: 66
+},
+{
+  id: 3,
+  description: 'test3',
+  QS: 20,
+  QF: 0,
+  PS: 10,
+  PF: 70
+}]
+
+
+var testRelationships = relationships.map((rel)=>Bayes.TestRelationship.create(rel));
+var modelState = Bayes.TestModelState.create(testRelationships)
 
 var Presentation = require('./presentation.js')
-var presentation = Presentation(coordinator);
+var presentation = Presentation(coordinator, modelState);
 var rendercontext = presentation.render(window.innerWidth,0.8*window.innerHeight,"#model");
 
 
--- src/js/presentation.js
+++ src/js/presentation.js
@@ -1,66 +1,199 @@
-var Bayes  = require('testistic-bayes')
-
-var relationships = [{
-  id: 1,
-  QS: 18,
-  QF: 2,
-  PS: 22,
-  PF: 58
-}, {
-  id: 2,
-  QS: 16,
-  QF: 4,
-  PS: 14,
-  PF: 66
-}]
-
-var solutionModel = Bayes.TestModelState.create(relationships)
-
-var roles = solutionModel.relationships;
-// var branchesModel = factory.model.branchesModel
-// var branches = branchesModel.getBranches();
-// var taskProducer = factory.model.taskProducer;
-
-function getRolesHierarchy(roles){
-  var root = {
-    id: "Roles",
-    name: "Roles",
-    children: []
-  }
-  root.children = roles.map(function(r){
-      return {
-      id: r.name,
-      name: r.name,
-      type:"Role",
-      children: []
-    }
-  })
-  return d3.hierarchy(root)
-}
-
-
-function Presentation(coordinator){
+function Presentation(coordinator, modelState){
   var TRANSITION_DURATION = 500;
   var OPACITY = {
-      NODE_DEFAULT: 0.7,
+      NODE_DEFAULT: 0.25,
       NODE_FADED: 0.0,
       NODE_HIGHLIGHT: 1,
       LINK_DEFAULT: 0.5,
       LINK_FADED: 0.0,
       LINK_HIGHLIGHT: 0.8
-    }
+  }
 
-  var svg
+  var nodesize=30
+
+  var svg, rootg
   var scaleX,scaleY;
 
   var renderContext;
 
-  var cline = d3.line().curve(d3.curveBundle.beta(0.85)).x(function(d){
-    return d.x;}).y(function(d){return d.y;})
+  var cline = d3.line().curve(d3.curveBundle.beta(0.85)).x(function(d){ return d.x;}).y(function(d){return d.y;})
 
  var tooltip
 
  var width,height
+
+ var viewState ='View Probability'
+
+ function midX(d){
+   return d.source.x + (d.target.x - d.source.x)/2 
+ }
+ function midY(d){
+  return d.source.y + (d.target.y - d.source.y)/2 
+ }
+ function textX(){
+  return nodesize*2/10
+ }
+ function textY(){
+  return nodesize*8/10
+ }
+
+function renderConfidence(nodes){
+  //nodes.append("text")
+  //   .attr("x",(d)=>d.x)
+  //   .attr("y",(d)=>d.y)
+  //   .text((d) => {
+  //     return d.data.name
+  //   });
+  nodes.append("rect")
+    .attr("x", function(d) {return d.x-nodesize })
+    .attr("y", function(d) {return d.y })
+    .attr("width", nodesize)
+    .attr("height", nodesize)
+    .on("click", mouseclickS)
+    .classed('selected', (d)=>{
+      return d.data.content.evidence==='S'
+    })
+     
+  nodes.append("text")
+    .attr("x",(d)=>d.x - nodesize + textX())
+    .attr("y",(d)=>d.y + textY())
+    .text((d) => {
+      var c = d.data.content
+      if(d.data.type==='modelState'){
+        var p = c.getProbabilities()
+        if(viewState==='View Probability'){
+          return p.pQ
+        }else {
+          return p.Q
+        }
+      }else {
+        if(viewState==='View Probability'){
+          return c.pQ
+        }else {
+          return c.Q
+        }
+        
+      }
+     })
+
+   nodes.append("rect")
+    .attr("x", function(d) {return d.x })
+    .attr("y", function(d) {return d.y })
+    .attr("width", nodesize)
+    .attr("height", nodesize)
+    .on("click", mouseclickF)
+    .classed('selected', (d)=>{
+      return d.data.content.evidence==='F'
+    })
+  nodes.append("text")
+    .attr("x",(d)=>d.x + textX())
+    .attr("y",(d)=>d.y + textY())
+    .text((d) => {
+      var c = d.data.content
+      if(d.data.type==='modelState'){
+        var p = c.getProbabilities()
+        if(viewState==='View Probability'){
+          return p.pP
+        }else {
+          return p.P
+        }
+      }else {
+        if(viewState==='View Probability'){
+          return c.pP
+        }else {
+          return c.P
+        }
+        
+      }
+     });
+}
+ function renderRelationship(links){
+    links.append("rect")
+      .classed('content', true)
+      .attr("x", function(d) {return midX(d)-nodesize })
+      .attr("y", function(d) {return midY(d)-nodesize })
+      .attr("width", nodesize)
+      .attr("height", nodesize)
+    links.append("text")
+      .attr("x",(d)=>midX(d) - nodesize + textX() )
+      .attr("y",(d)=>midY(d) - nodesize + textY() )
+      .text((d) => {
+        if(viewState==='View Probability'){
+          return d.target.data.content.pQS
+        }else {
+          return d.target.data.content.QS
+        }
+    })
+
+    links.append("rect")
+      .classed('content', true)
+      .attr("x", function(d) {return midX(d)-nodesize })
+      .attr("y", function(d) {return midY(d) })
+      .attr("width", nodesize)
+      .attr("height", nodesize)
+    links.append("text")
+      .attr("x",(d)=>midX(d) - nodesize + textX() )
+      .attr("y",(d)=>midY(d) + textY() )
+      .text((d) => {
+        if(viewState==='View Probability'){
+          return d.target.data.content.pQF
+        }else {
+          return d.target.data.content.QF
+        }
+    })
+      
+    links.append("rect")
+      .classed('content', true)
+      .attr("x", function(d) {return midX(d) })
+      .attr("y", function(d) {return midY(d)-nodesize })
+      .attr("width", nodesize)
+      .attr("height", nodesize)
+    links.append("text")
+      .attr("x",(d)=>midX(d)  + textX() )
+      .attr("y",(d)=>midY(d) - nodesize + textY() )
+      .text((d) => {
+        if(viewState==='View Probability'){
+          return d.target.data.content.pPS
+        }else {
+          return d.target.data.content.PS
+        }
+    })
+
+    links.append("rect")
+      .classed('content', true)
+      .attr("x", function(d) {return midX(d) })
+      .attr("y", function(d) {return midY(d) })
+      .attr("width", nodesize)
+      .attr("height", nodesize)
+    links.append("text")
+      .attr("x",(d)=>midX(d)  + textX() )
+      .attr("y",(d)=>midY(d)  + textY() )
+      .text((d) => {
+        if(viewState==='View Probability'){
+          return d.target.data.content.pPF
+        }else {
+          return d.target.data.content.PF
+        }
+    })
+  }
+
+  function getHierarchy(modelState){
+    var root = {
+      id: "model",
+      name: "Quality",
+      type: "modelState",
+      content: modelState
+    }
+    root.children = modelState.relationships.map(function(r){
+        return {
+        id: r.id,
+        name: r.name,
+        type: "relationship",
+        content: r
+      }
+    })
+    return d3.hierarchy(root)
+  }
 
   function navigate(node) {
     window.location = "#"+ node.id;
@@ -102,24 +235,9 @@ function Presentation(coordinator){
     }
 
     function mouseovered(node) {
-      if(node.data.type=="Role" && node.data.name=='SolutionOwner')
-        tooltip.html("<p>Double click to add new feature</p>");
-      else if(node.data.type=="Role" && node.data.name=='SolutionArchitect')
-        tooltip.html("<p>Drag to feature branch to define and add development tasks</p>");
-      else if(node.data.type=="Role" && node.data.name=='QA')
-          tooltip.html("<p>Drag to QA branch to indicate the QA branch has passed regression</p>");
-      else if(node.data.type=="Role" && node.data.name=='Developer')
-          tooltip.html("<p>Drag over a development task to complete development task</p>");
-      else if(node.data.type=="Task")
-          tooltip.html("<p>"+node.data.name+"</p>");
-      else if(node.data.type=="Task")
-          tooltip.html("<p>"+node.data.name+"</p>");
-      else if(node.data.type=="Branch" && node.data.name=='QA')
-          tooltip.html("<p>Features:"+node.data.features+"</p><p>Regression Tested:"+node.data.verified+"</p>");
-
-      else if(node.data.type=="Branch")
-          tooltip.html("<p>Features:"+node.data.features+"</p><p>Tasks:"+Object.keys(node.data.tasks).length+"</p>");
-
+      
+      tooltip.html(`<p>${node.data.content.description}</p>`);
+     
       showTooltip();
     }
     function mouseSelected(node) {
@@ -133,92 +251,29 @@ function Presentation(coordinator){
     dragy=node.y
   }
   function enddrag(d){
-    //var nd = d3.select(this)
-    if(d.data.name=='Customer'){
-      identifyBranch(d3.event,function(branch,tasks){
-        branch.completeFeature();
-      })
-    }
-    else if(d.data.name=='SolutionArchitect'){
-      identifyBranch(d3.event,function(branch,tasks){
-        branch.addTasks(tasks);
-      })
-    }
-    else if(d.data.name=='QA'){
-      identifyBranch(d3.event,function(branch,tasks){
-        if(branch.name=='QA')
-          branch.verify();
-      })
-    }
-    else if(d.data.name=='DevOps'){
-      identifyBranch(d3.event,function(branch,tasks){
-        if(branch.name=='QA')
-          branchesModel.release();
-      })
-    }
-    else if(d.data.name=='Developer'){
-      var cx1 = d3.event.x;
-      var cy1 = d3.event.y;
-
-      var taskSelection = d3.selectAll(".task");
-      var circleSelection = taskSelection.select("circle");
-      circleSelection.each(function(cir){
-          var cx2 = cir.x;
-          var cy2 = cir.y;
-          if(collide([cx1,cy1],10,[cx2,cy2],10)){
-            var taskName = cir.data.name
-            var branch = branchesModel.findTaskBranch(taskName)
-            var task = branch.findTask(taskName)
-            branch.completeTask(task)
-            renderContext.update();
-          }
-      });
-    }
-    d3.select(this).attr("transform",null)
   }
   function dragged(d){
     var dt = "translate("+ (d3.event.x-dragx)+","+(d3.event.y-dragy)+")"
     d3.select(this).attr("transform",dt);
   }
 
-  function identifyBranch(event,action){
-    var cx1 = d3.event.x;
-    var cy1 = d3.event.y;
-    var branchSelection = d3.selectAll(".branch");
-    var circleSelection = branchSelection.select("circle");
-    circleSelection.each(function(cir){
-        var cx2 = cir.x;
-        var cy2 = cir.y
-        ;
-        if(collide([cx1,cy1],10,[cx2,cy2],10)){
-          var branchName = cir.data.name
-          var tasks = taskProducer.createTasks(2);
-          var branch = branchesModel.getBranch(branchName)
-          action(branch,tasks)
-          //branch.addTasks(tasks);
-          renderContext.update();
-        }
-    });
+  function mouseclickS(contextNode){
+    var testresult = {
+      id: contextNode.data.id,
+      S: true
+    }
+    modelState.updateGiven(testresult)
+    renderContext.update();
   }
 
-  function collide(p1,r1,p2,r2){
-    var colliding=false;
-
-    var ax1=p1[0]-r1
-    var ax2=p1[0]+r1
-    var ay1=p1[1]-r1
-    var ay2=p1[1]+r1
-
-    var bx1=p2[0]-r2
-    var bx2=p2[0]+r2
-    var by1=p2[1]-r2
-    var by2=p2[1]+r2
-
-    if(!(ax1 > bx2 || ax2 < bx1 || ay1 > by2 || ay2 < by1))
-      colliding=true;
-
-    return colliding;
-}
+  function mouseclickF(contextNode){
+    var testresult = {
+      id: contextNode.data.id,
+      F: true
+    }
+    modelState.updateGiven(testresult)
+    renderContext.update();
+  }
 
   renderContext = {
 
@@ -226,7 +281,9 @@ function Presentation(coordinator){
       width=dwidth
       height=dheight
 
-        var chart = d3.select(divselect)
+      var presentation = this
+
+      var chart = d3.select(divselect)
         svg = chart.append("svg")
         .attr("width", width)
         .attr("height", height*0.75)
@@ -236,6 +293,18 @@ function Presentation(coordinator){
         scaleX = d3.scaleLinear().domain([0,1]).range([0, width])
         scaleY = d3.scaleLinear().domain([0,1]).range([0,height])
 
+        d3.select("#toggle").on("click", (d)=>{
+          if(viewState==='View Probability'){
+            viewState = 'View Frequency'
+          }else {
+            viewState = 'View Probability'
+          }
+          d3.select("#toggle").text(viewState)
+          presentation.update();
+        })
+
+    
+
         this.update();
 
         return renderContext;
@@ -244,51 +313,62 @@ function Presentation(coordinator){
     update: async function(){
 
       svg.selectAll("g").remove();
+      rootg = svg.append("g").attr("transform","translate(0,100)")
 
-      var g = svg.append("g").attr("transform","translate(0,0)")
+      var modelHierarchy = getHierarchy(modelState)
 
-      var rolesHierarchy = getRolesHierarchy(roles)
-
+      var rolesTree = d3.cluster().size([width,height*0.40])
+      rolesTree(modelHierarchy)
     
-      var rolesTree = d3.cluster().size([width,height*0.10])
-      rolesTree(rolesHierarchy)
-      var roleLeaves = rolesHierarchy.leaves();
+      {
+        var links = modelHierarchy.links();
+        var ds = rootg.selectAll(".link").data(links,(d)=>{
+          return d.source.data.id + d.target.data.id
+        })
+        
+        var ln = ds.enter();
 
-      var rn = g.selectAll(".role").data(roleLeaves,(d)=>{
-        return d.data.id;
-      }).enter();
+        var l = ln.append("g")
+        //.on("click", mouseclick)
+        .on("mouseover", mouseovered)
+        .on("mouseout", mouseouted)
+        .classed("link", true)
+        .classed('content', true)
+        l.append('line')
+          .classed('link', true)
+          .attr('x1', function(d) {return d.source.x;})
+          .attr('y1', function(d) {return d.source.y;})
+          .attr('x2', function(d) {return d.target.x;})
+          .attr('y2', function(d) {return d.target.y;});
 
-      var d = rn.append("g")
-        .attr("class", (d)=>"role")
+        renderRelationship(l)
+      }
+      {
+        var modelNodes = modelHierarchy.descendants();
+        var ds = rootg.selectAll(".node").data(modelNodes,(d)=>{
+          return d.data.id;
+        });
+        
+        var rn = ds.enter();
 
-      d.call(d3.drag()
-            .on("start",startdrag)
-            .on("drag",dragged)
-            .on("end",enddrag));
-
-      d.append("circle")
-          .attr("r",20)
-          .attr("cx",(d)=>d.x)
-          .attr("cy",(d)=>d.y)
-          .attr("class", (d)=>"role-circle")
-          .on("click", mouseclick)
+        var d = rn.append("g")
+          .attr("id", (d)=>{
+            return "N"+d.data.id
+          })
+          // .on("click", mouseclick)
           .on("mouseover", mouseovered)
           .on("mouseout", mouseouted)
+          .classed("node", true)
+          .classed('content', true)
+          
+        // d.call(d3.drag()
+        //       .on("start",startdrag)
+        //       .on("drag",dragged)
+        //       .on("end",enddrag));
 
-      d.append("text")
-          .attr("x",(d)=>d.x)
-          .attr("y",(d)=>d.y)
-          .text((d) => {
-            return d.data.name
-          });
-
-   },
-
-  function mouseclick(contextNode){
-    if(contextNode.data.name=='SolutionOwner'){
-      solutionModel.makeFeature()
-    }
-    renderContext.update();
+        renderConfidence(d)  
+      }
+    },
   }
 
   return renderContext;
